c39c47c55c2e28321e3a4c3e8469f1a3c4d6c6ab
Dawid Jasiński <ambas90@gmail.com>
2023-11-19 13:22:34 +0100

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+//inicjalizacja odtwarzacza vimeo
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+//funkcja zapisująca aktualny czas odtwarzania
+function getCurrentTime(data) {
+  const currentTime = data;
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}
+//śledzenie zdarzenia timeupdate - aktualizacji czasu odtwarzania
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+//wznaweianie odtwarzania od ostatnio zapisanego momentu
+window.addEventListener('DOMContentLoaded', () => {
+  try {
+    const savedTime = JSON.parse(
+      localStorage.getItem('videoplayer-current-time')
+    ).seconds;
+    console.log(savedTime);
+    player.setCurrentTime(savedTime);
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+});
