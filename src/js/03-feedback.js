cff32940b0d55c0bb0fe8a0ce3ec6de33aee0bdf
Dawid Jasiński <ambas90@gmail.com>
2023-11-19 17:50:18 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const email = document.querySelector('input[name = email]');
+const message = document.querySelector('textarea[name = message]');
+const form = document.querySelector('.feedback-form');
+const feedbackFormState = { email, message };
 
+//nasłuchiwanie wpisywania tekstu
+form.addEventListener('input', event => {
+  feedbackFormState[event.target.name] = event.target.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify(feedbackFormState)
+  );
+});
+
+//czyszczenie storage i pól formularza
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(feedbackFormState);
+  localStorage.removeItem('feedback-form-state');
+  email.value = null;
+  message.value = null;
+});
+
+// uzupełnianie formularza podczas przeładowania strony
+window.addEventListener('DOMContentLoaded', () => {
+  try {
+    email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+    message.value = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    ).message;
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+});
