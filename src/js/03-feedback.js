53475cb9d51127d7644b21700b8841e7cd3ceaed
Dawid Jasiński <ambas90@gmail.com>
2023-11-19 18:09:56 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,20 @@ const message = document.querySelector('textarea[name = message]');
 const form = document.querySelector('.feedback-form');
 const feedbackFormState = { email, message };
 
-//nasłuchiwanie wpisywania tekstu
-form.addEventListener('input', event => {
+//zapisywanie teksu pół formularza
+function formSaver(event) {
+  feedbackFormState.email = email.value;
+  feedbackFormState.message = message.value;
   feedbackFormState[event.target.name] = event.target.value;
+  console.log(feedbackFormState);
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify(feedbackFormState)
   );
-});
+}
+
+//nasłuchiwanie wpisywania tekstu
+form.addEventListener('input', throttle(formSaver, 500));
 
 //czyszczenie storage i pól formularza
 form.addEventListener('submit', event => {
